e8cda3e592d4ef7b05956694433c9c4a7fd5b805
aniversarioperu <aniversarioperu1@gmail.com>
2014-03-28 17:20:47 +0200

    user agent

--- manolo.py
+++ manolo.py
@@ -14,6 +14,23 @@ import codecs
 from bs4 import BeautifulSoup
 from datetime import date, timedelta as td
 
+USER_AGENTS = [
+	"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; Crazy Browser 1.0.5)",
+	"curl/7.7.2 (powerpc-apple-darwin6.0) libcurl 7.7.2 (OpenSSL 0.9.6b)",
+	"Mozilla/5.0 (X11; U; Linux amd64; en-US; rv:5.0) Gecko/20110619 Firefox/5.0",
+	"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b8pre) Gecko/20101213 Firefox/4.0b8pre",
+	"Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)",
+	"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 7.1; Trident/5.0)",
+	"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0) chromeframe/10.0.648.205",
+	"Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET CLR 2.0.50727)",
+	"Opera/9.80 (Windows NT 6.1; U; sv) Presto/2.7.62 Version/11.01",
+	"Opera/9.80 (Windows NT 6.1; U; pl) Presto/2.7.62 Version/11.00",
+	"Opera/9.80 (X11; Linux i686; U; pl) Presto/2.6.30 Version/10.61",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.861.0 Safari/535.2",
+	"Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.872.0 Safari/535.2",
+	"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.812.0 Safari/535.1",
+	]
+
 def html_to_csv(html):
     # taken from http://stackoverflow.com/a/14167916
     soup = BeautifulSoup(html)
@@ -66,7 +83,8 @@ def buscar(fecha):
     payload = {
             "VisitaConsultaQueryForm[feConsulta]": fecha
             }
-    r = requests.post(url, data=payload)
+    headers = { "User-Agent": USER_AGENTS[randint(0, len(USER_AGENTS))-1]}
+    r = requests.post(url, data=payload, headers=headers)
     r.encoding = "utf8"
     csv = html_to_csv(r.text)
     print url
@@ -82,7 +100,7 @@ def buscar(fecha):
             print url
             try:
                 sleep(randint(5,15))
-                r = requests.post(url, data=payload)
+                r = requests.post(url, data=payload, headers=headers)
                 r.encoding = "utf8"
                 csv = html_to_csv(r.text)
             except:
