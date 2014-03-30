98f105cf00441f4ed416c952d624199955a7e708
aniversarioperu <aniversarioperu1@gmail.com>
2014-03-30 10:02:15 +0300

    input html output json to file

--- a.py
+++ a.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 import os
 
 def html_to_json(html):
+    import json
+
     # taken from http://stackoverflow.com/a/14167916
     soup = BeautifulSoup(html)
     table = soup.find('table', attrs={'class': 'items'})
@@ -13,16 +15,28 @@ def html_to_json(html):
     for row in table.find_all('tr'):
         rows.append([val.text for val in row.find_all('td')])
 
-    filename = os.path.join(config.base_folder, "output.csv")
+    filename = os.path.join(config.base_folder, "output.json")
     f = codecs.open(filename, "a", "utf8")
     for i in rows:
         if len(i) > 1:
-            out  = i[0] + "|" + i[1] + "|" + i[2] + "|" + i[3] + "|"
-            out += i[4] + "|" + i[5] + "|" + i[6] + "|" + i[7] + "|"
-            out += i[8] + "|" + i[9] + "\n"
-            f.write(out)
+            out = dict()
+            out['date'] = i[0].strip()
+            out['visitor'] = i[1].strip()
+            out['id_document'] = i[2].strip()
+            out['entity'] = i[3].strip()
+            out['objective'] = i[4].strip()
+            out['host'] = i[5].strip()
+            out['office'] = i[6].strip()
+            out['meeting_place'] = i[7].strip()
+            out['time_start'] = i[8].strip()
+            try:
+                out['time_end'] = i[9].strip()
+            except:
+                out['time_end'] = ""
+
+            f.write(json.dumps(out) + "\n")
         else:
-            print i
+            print ""
     f.close()
 
 
