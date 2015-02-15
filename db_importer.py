354f261429269e9a73872bdc816cf1a0a2e738f5
aniversarioperu <aniversarioperu1@gmail.com>
2015-02-15 02:59:50 +0200

    asume clean dni_number

--- db_importer.py
+++ db_importer.py
@@ -30,12 +30,12 @@ def get_tables_from_source_db(input_db):
 
 
 class Importer(object):
-    def __init__(self, input_db, settings, limit, verbosity):
+    def __init__(self, input_json, settings, limit, verbosity):
         self.items = []
         self.mytable = ''
         self.verbosity = verbosity
         self.limit = limit
-        self.input_db = input_db
+        self.input_json = input_json
         self.settings = settings
         self.ids_seen = self.get_ids_from_db()
 
@@ -58,29 +58,8 @@ class Importer(object):
                 ids_seen.add(i['sha1'])
         return ids_seen
 
-    def get_table(self, mytable):
-        self.mytable = mytable
-
-        db = dataset.connect("sqlite:///" + self.input_db)
-
-        if self.limit:
-            res = db.query("select * from " + mytable + " limit %i" % limit)
-        else:
-            res = db.query("select * from " + mytable)
-
-        for item in res:
-            item = self.parse_item(item)
-
-        if item['sha1'] in self.ids_seen:
-            if self.verbosity:
-                print("Duplicate item found: %s" % item['sha1'])
-                print(item)
-        else:
-            self.ids_seen.add(item['sha1'])
-            self.items.append(item)
-
     def import_json(self):
-        with open(self.input_db) as handle:
+        with open(self.input_json) as handle:
             for line in handle:
                 item = line.strip()
                 item = self.parse_item(json.loads(item))
@@ -130,11 +109,6 @@ class Importer(object):
         if 'observation' in item:
             del item['observation']
 
-        if 'id_number' in item:
-            item['id_document'], item['id_number'] = self.get_dni(item['id_number'])
-        else:
-            item['id_document'], item['id_number'] = self.get_dni(item['id_document'])
-
         if 'meeting_place' not in item:
             item['meeting_place'] = ''
 
@@ -150,25 +124,6 @@ class Importer(object):
             item['time_start']
         )
         return item
-
-    def get_dni(self, document_identity):
-        id_document = ''
-        id_number = ''
-
-        document_identity = document_identity.replace(':', ' ')
-        document_identity = re.sub('\s+', ' ', document_identity)
-        document_identity = document_identity.strip()
-        document_identity = re.sub('^', ' ', document_identity)
-
-        res = re.search("(.*)\s(([A-Za-z0-9]+\W*)+)$", document_identity)
-        if res:
-            id_document = res.groups()[0].strip()
-            id_number = res.groups()[1].strip()
-
-        if id_document == '':
-            id_document = 'DNI'
-
-        return id_document, id_number
 
     def bulk_upload(self):
         items_to_upload = []
@@ -221,18 +176,8 @@ def make_hash(institution, full_name, id_document, id_number, date, time_start):
     return hash_output.hexdigest()
 
 
-def import_data(input_db, settings, limit, verbosity):
-    for mytable in get_tables_from_source_db(input_db):
-        if mytable.startswith("manolo"):
-            importer = Importer(input_db, settings, limit, verbosity)
-            importer.get_table(mytable)
-
-            print("%i items will be imported for table %s" % (len(importer.items), mytable))
-            importer.import_table()
-
-
-def import_from_json(input_db, settings, limit, verbosity):
-    importer = Importer(input_db, settings, limit, verbosity)
+def import_from_json(input_json, settings, limit, verbosity):
+    importer = Importer(input_json, settings, limit, verbosity)
     importer.import_json()
     importer.bulk_upload()
 
