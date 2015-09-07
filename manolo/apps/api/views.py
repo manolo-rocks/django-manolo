3a1f17d2346faa20705a86d96cf5583bd9321fce
aniversarioperu <yoni@aniversarioperu.me>
2015-09-07 23:20:18 +0300

    start pagination

--- manolo/apps/api/views.py
+++ manolo/apps/api/views.py
@@ -4,9 +4,11 @@ from django.http.request import QueryDict
 from rest_framework.decorators import api_view
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny
+from rest_framework.pagination import PageNumberPagination
 
 from .forms import ApiForm
 from .serializers import ManoloSerializer
+from .api_responses import JSONResponse
 
 
 @api_view(['GET'])
@@ -16,6 +18,7 @@ def search(request, query):
     form = ApiForm(query_request)
     results = [i.object for i in form.search()]
     serializer = ManoloSerializer(results, many=True)
-    print(serializer.data)
 
-    return render(request, "search/search.html")
+    page = PageNumberPagination()
+    results = page.paginate_queryset(serializer.data, request)
+    return JSONResponse(results)
