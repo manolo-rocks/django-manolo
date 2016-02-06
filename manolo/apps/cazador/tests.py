303bf95ff19972121fa9eb3a78c2781148474fb5
carlosp420 <mycalesis@gmail.com>
2016-02-06 01:12:56 -0500

    shrink url in raw data match

--- manolo/apps/cazador/tests.py
+++ manolo/apps/cazador/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import unittest
 
-# Create your tests here.
+from .utils import shrink_url_in_string
+
+
+class TestUtils(unittest.TestCase):
+    def setUp(self):
+        self.maxDiff = None
+
+    def test_shrink_url_in_string(self):
+        my_string = "Carlos Fernando Raffo Arce http://ot.minjus.gob.pe:8080/sisca_web/DeudoresWebAction_verDeudorWeb.action?deudor.id=7683"
+        expected = "Carlos Fernando Raffo Arce  <a href='http://ot.minjus.gob.pe:8080/sisca_web/DeudoresWebAction_verDeudorWeb.action?deudor.id=7683'>http://ot.minjus.gob.pe...</a>"
+        result = shrink_url_in_string(my_string)
+        self.assertEqual(expected, result)
