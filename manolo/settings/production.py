81fd1a4165c7ca1f2e5478b4fef34022998c503f
AniversarioPeru <aniversarioperu1@gmail.com>
2020-11-14 11:12:37 -0500

    add docker

--- manolo/settings/production.py
+++ manolo/settings/production.py
@@ -2,10 +2,6 @@ from .base import *  # noqa
 from .base import env
 
 
-SECRET_KEY = env("DJANGO_SECRET_KEY")
-
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=['manolo.rocks'])
-
 STATIC_ROOT = "/var/www/manolo/static/"
 MEDIA_ROOT = "/var/www/manolo/media/"
 
@@ -18,6 +14,5 @@ ALLOWED_HOSTS = [
     '.manolo.rocks.',  # Also allow FQDN and subdomains
 ]
 
-DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
 DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
