e82b2afb50f8f0a68c8248ca476dd616fddf3c16
Lisbeth S <lisbeth.s4.wasp@gmail.com>
2020-11-27 01:07:35 -0500

    fixed defensa, mincu, minsa

--- scrapers/manolo_scraper/spiders/inpe.py
+++ scrapers/manolo_scraper/spiders/inpe.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+import json
+
 import scrapy
+from scrapy import Request, FormRequest
 
 from scrapers.manolo_scraper.spiders.spiders import ManoloBaseSpider
 from ..item_loaders import ManoloItemLoader
@@ -10,21 +13,63 @@ from ..utils import make_hash
 class INPESpider(ManoloBaseSpider):
     name = 'inpe'
     allowed_domains = [
-        'www.peru.gob.pe',
-        'visitasadm.inpe.gob.pe'
+        'apps.inpe.gob.pe',
     ]
 
     def initial_request(self, date):
         date_str = date.strftime('%d/%m/%Y')
-        request = scrapy.FormRequest('http://visitasadm.inpe.gob.pe/VisitasadmInpe/Controller',
-                                     formdata={'vis_fec_ing': date_str},
-                                     meta={'date': date_str},
-                                     callback=self.parse,
-                                     )
+        url = f'https://apps.inpe.gob.pe/VisitasadmInpe/CargarTablaVisitaJSON?FechaVisita={date_str}&local=-1'
+        requests = Request(
+            url,
+            meta={'date': date_str},
+            dont_filter=True,
+            callback=self.parse_initial_request,
+        )
+        print('requests', requests)
+        return requests
 
-        return request
+    def parse_initial_request(self, response):
+        date = response.meta['date']
+        data = json.loads(response.text)
+        record_count = data['iTotalDisplayRecords']
 
-    def parse(self, response):
+        steps = list(range(0, record_count, 8))
+        requests = []
+        for i in steps:
+            print(i, response.url)
+            params = {'start': i, 'length': 8}
+            print(params)
+            request = FormRequest.from_response(
+                response,
+                formdata=params,
+                dont_filter=True,
+                callback=self.parse,
+            )
+            requests.append(request)
+        return requests
+
+    def _request_initial_date_page(self, response, date_str, callback):
+        data = json.loads(response.text)
+        record_count = data['iTotalDisplayRecords']
+
+        steps = list(range(0, record_count, 8))
+
+        for i in steps:
+            print(i, response.url)
+            params = {'start': i, 'length': 8}
+            print(params)
+            request = FormRequest.from_response(
+                response,
+                formdata=params,
+                callback=self.hola,
+            )
+            yield request
+
+    def hola(self, response):
+        print('hola')
+
+    def parse(self, response, **kwargs):
+        print('**** response')
         date = self.get_date_item(response.meta['date'], '%d/%m/%Y')
 
         rows = response.xpath('//tr')
@@ -52,7 +97,7 @@ class INPESpider(ManoloBaseSpider):
                 l.add_xpath('host_name', './td[8]/p/text()')
 
                 # Add conditional, don't accept "---"
-                l.add_xpath('title', './td[9]/p/text()')
+                l.add_xpath('host_title', './td[9]/p/text()')
 
                 l.add_xpath('office', './td[10]/p/text()')
 
