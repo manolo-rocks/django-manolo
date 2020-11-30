82dd3db6cfc250db0a0b09237170def2233bbd6d
Lisbeth S <lisbeth.s4.wasp@gmail.com>
2020-11-29 20:12:20 -0500

    progress

--- visitors/tasks.py
+++ visitors/tasks.py
@@ -1,15 +1,19 @@
 from datetime import timedelta
 import logging
 
+from celery.canvas import chain
 from celery.task import periodic_task
 from django.db import OperationalError
 
+from manolo.celery import app
+from scrapers.manolo_scraper.spiders.ambiente import AmbienteSpider
+from scrapers.manolo_scraper.spiders.congreso import CongresoSpider
 from visitors.management.commands.run_statistics import run_statistics
 
 log = logging.getLogger(__name__)
 
 
-@periodic_task(run_every=timedelta(minutes=60 * 24))
+@periodic_task(run_every=timedelta(minutes=60 * 25))
 def run_stats() -> None:
     log.info("Recompute stats")
     try:
@@ -22,4 +26,21 @@ def run_stats() -> None:
 def schedule_spiders() -> None:
     # for spider in spiders
     # schedule crawl with delay (create a celery chain)
-    pass
+    spiders = [
+        AmbienteSpider,
+        CongresoSpider,
+    ]
+
+    tasks = []
+
+    for spider in spiders:
+        task = schedule_crawl.si(spider.name)
+        tasks.append(task)
+
+    task_chain = chain(*tasks)
+    task_chain.apply_async()
+
+
+@app.task
+def schedule_crawl(spider_name: str) -> None:
+    print(spider_name)
