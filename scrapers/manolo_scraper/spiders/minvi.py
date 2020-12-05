49918c0c8b6fe7e9a885e35b2f89f69f058cb237
AniversarioPeru <aniversarioperu1@gmail.com>
2020-12-05 12:03:12 -0500

    fixed minvi

--- scrapers/manolo_scraper/spiders/minvi.py
+++ scrapers/manolo_scraper/spiders/minvi.py
@@ -5,8 +5,8 @@ from scrapers.manolo_scraper.spiders.spiders import SireviSpider
 class MinviSpider(SireviSpider):
     name = 'minvi'
 
-    allowed_domains = ['geo.vivienda.gob.pe']
+    allowed_domains = ['s01pweb001.vivienda.gob.pe']
 
-    base_url = 'http://geo.vivienda.gob.pe/Visitas/controlVisitas'
+    base_url = "http://s01pweb001.vivienda.gob.pe/Visitas/controlVisitas/index.php"
 
     institution_name = 'vivienda'
