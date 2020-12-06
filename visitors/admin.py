f208426062d0ed914037155da52d34baf46dce5a
AniversarioPeru <aniversarioperu1@gmail.com>
2020-12-06 14:09:18 -0500

    show last updated

--- visitors/admin.py
+++ visitors/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from visitors.models import Subscriber, Visitor, Developer
+from visitors.models import Subscriber, Visitor, Developer, Institution
 
 
 # Define an inline admin descriptor for Subscriber model
@@ -17,8 +17,14 @@ class SubscriberInline(admin.StackedInline):
 class UserAdmin(BaseUserAdmin):
     inlines = (SubscriberInline, )
 
+
+class InstitutionAdmin(admin.ModelAdmin):
+    model = Institution
+
+
 # Re-register UserAdmin
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+admin.site.register(Institution, InstitutionAdmin)
 admin.site.register(Visitor)
 admin.site.register(Developer)
