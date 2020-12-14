fde1cad3bcffbe233e1716df2ae0045d45af5a7c
AniversarioPeru <aniversarioperu1@gmail.com>
2020-12-13 20:15:55 -0500

    fixed produce

---
+++ scrapers/manolo_scraper/spiders/produce2.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+import json
+
+import scrapy
+
+from .spiders import ManoloBaseSpider
+from ..item_loaders import ManoloItemLoader
+from ..items import ManoloItem
+from ..utils import make_hash
+
+
+class ProduceSpider(ManoloBaseSpider):
+    name = 'produce'
+    allowed_domains = ['www.produce.gob.pe']
+
+    def initial_request(self, date):
+        date_str = date.strftime("%d/%m/%Y")
+        display_end = 10_000
+        url = (
+            'https://www.produce.gob.pe/images/produce/transparencia/visitas/ajax/visitas.php?'
+            'sEcho=4&iColumns=13&sColumns=%2C%2C%2C%2C%2C%2C%2C%2C%2C%2C%2C%2C&iDisplayStart=0&'
+            f'iDisplayLength={display_end}&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true'
+            '&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&mDataProp_2=2&sSearch_2=&'
+            'bRegex_2=false&bSearchable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&'
+            'bSearchable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&'
+            'mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&mDataProp_6=6&sSearch_6=&'
+            'bRegex_6=false&bSearchable_6=true&mDataProp_7=7&sSearch_7=&bRegex_7=false&'
+            'bSearchable_7=true&mDataProp_8=8&sSearch_8=&bRegex_8=false&bSearchable_8=true&'
+            'mDataProp_9=9&sSearch_9=&bRegex_9=false&bSearchable_9=true&mDataProp_10=10&sSearch_10=&'
+            'bRegex_10=false&bSearchable_10=true&mDataProp_11=11&sSearch_11=&bRegex_11=false&'
+            'bSearchable_11=true&mDataProp_12=12&sSearch_12=&bRegex_12=false&bSearchable_12=true&'
+            'sSearch=&bRegex=false&dni=&visitante=&entidad=&funcionario=&sede=0&motivo=0&'
+            f'fecha_inicio={date_str}&fecha_fin={date_str}&_=1607905978616'
+        )
+        request = scrapy.Request(url, callback=self.parse)
+        request.meta['date'] = date_str
+
+        # formdata={
+            #     'fecha_inicio': date_str,
+            #     'fecha_fin': date_str,
+            #     'sEcho': '4',
+            #     'iColumns': '13',
+            #     'sColumns': ',,,,,,,,,,,,',
+            #     'iDisplayStart': '0',
+            #     'iDisplayLength': '100',
+            #     'mDataProp_0': '0',
+            #     'sSearch_0':  '',
+            #     'bRegex_0': 'false',
+            #     'bSearchable_0': 'true',
+            #     'mDataProp_1': '1',
+            #     'sSearch_1': '',
+            #     'bRegex_1': 'false',
+            #     'bSearchable_1': 'true',
+            #     'mDataProp_2': '2',
+            #     'sSearch_2': '',
+            #     'bRegex_2': 'false',
+            #     'bSearchable_2': 'true',
+            #     'mDataProp_3': '3',
+            #     'sSearch_3': '',
+            #     'bRegex_3': 'false',
+            #     'bSearchable_3': 'true',
+            #     'mDataProp_4': '4',
+            #     'sSearch_4': '',
+            #     'bRegex_4': 'false',
+            #     'bSearchable_4': 'true',
+            #     'mDataProp_5': '5',
+            #     'sSearch_5': '',
+            #     'bRegex_5': 'false',
+            #     'bSearchable_5': 'true',
+            #     'mDataProp_6': '6',
+            #     'sSearch_6': '',
+            #     'bRegex_6': 'false',
+            #     'bSearchable_6': 'true',
+            #     'mDataProp_7': '7',
+            #     'sSearch_7': '',
+            #     'bRegex_7': 'false',
+            #     'bSearchable_7': 'true',
+            #     'mDataProp_8': '8',
+            #     'sSearch_8': '',
+            #     'bRegex_8': 'false',
+            #     'bSearchable_8': 'true',
+            #     'mDataProp_9': '9',
+            #     'sSearch_9': '',
+            #     'bRegex_9': 'false',
+            #     'bSearchable_9': 'true',
+            #     'mDataProp_10': '10',
+            #     'sSearch_10': '',
+            #     'bRegex_10': 'false',
+            #     'bSearchable_10': 'true',
+            #     'mDataProp_11': '11',
+            #     'sSearch_11': '',
+            #     'bRegex_11': 'false',
+            #     'bSearchable_11': 'true',
+            #     'mDataProp_12': '12',
+            #     'sSearch_12': '',
+            #     'bRegex_12': 'false',
+            #     'bSearchable_12': 'true',
+            #     'sSearch': '',
+            #     'bRegex': 'false',
+            #     'dni': '',
+            #     'visitante': '',
+            #     'entidad': '',
+            #     'funcionario': '',
+            #     'sede': '0',
+            #     'motivo': '0',
+            #     '_': '1607905978616',
+            # },
+
+        return request
+
+    def parse(self, response, **kwargs):
+        print('***', response)
+        with open('a.html', 'w') as handle:
+            handle.write(response.text)
+        date = self.get_date_item(response.meta['date'], '%d/%m/%Y')
+        response_json = json.loads(response.text)
+        rows = response_json['aaData']
+
+        for row in rows:
+            print(row)
+            l = ManoloItemLoader(item=ManoloItem(), selector=row)
+
+            l.add_value('institution', 'produce')
+            l.add_value('date', date),
+            l.add_value('full_name', row['VISITANTE'])
+            try:
+                l.add_value('id_document', row['DOCUMENTO'].split(' ')[0])
+            except:
+                l.add_value('id_document', 'DNI')
+
+            l.add_value('id_number', row['NUMERO_DOCUMENTO'])
+            l.add_value('reason', row['MOTIVO'])
+            l.add_value('entity', row['ENTIDAD'])
+            l.add_value('host_name', row['FUNCIONARIO'])
+            l.add_value('office', row['DEPENDENCIA'])
+            l.add_value('time_end', row['HORA_SALIDA'])
+            l.add_value('time_start', row['HORA_INGRESO'])
+            l.add_value('location', row['SEDE'])
+            l.add_value('host_title', row['DESCRIPCION_CARGO'])
+
+            item = l.load_item()
+            item = make_hash(item)
+
+            yield item
