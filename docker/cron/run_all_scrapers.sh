677c8a4597efef9d810fa66da805e9a5bb8428bc
AniversarioPeru <aniversarioperu1@gmail.com>
2020-12-19 08:51:40 -0500

    run all scrapers

---
+++ docker/cron/run_all_scrapers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+last_week=$(date --date='7 days ago' +"%Y-%m-%d")
+today=$(date +"%Y-%m-%d")
+
+scrapy_path=/code/scrapers/manolo_scraper
+scrapy_bin=/usr/local/bin/scrapy
+
+#cd $scrapy_path
+
+declare -a scrapers=(
+    'ambiente' 'congreso' 'defensa' 'ingemmet' 'inpe' 'minagr' 'mincetur'
+    'mincu' 'minedu' 'minem' 'minsa' 'minvi' 'osce' 'pcm' 'presidencia' 'produce2'
+)
+
+for i in "${scrapers[@]}"; do
+  $scrapy_bin crawl $i -a date_start=$last_week -a date_end=$today
+done
