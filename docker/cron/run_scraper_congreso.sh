ff25f267664b25416eb694d9265227c6d538a620
AniversarioPeru <aniversarioperu1@gmail.com>
2021-08-21 10:50:48 -0500

    simplify scraper cron

--- docker/cron/run_scraper_congreso.sh
+++ docker/cron/run_scraper_congreso.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-last_week=$(date --date='7 days ago' +"%Y-%m-%d")
+last_week=$(date --date="${1} days ago" +"%Y-%m-%d")
 today=$(date +"%Y-%m-%d")
 
 scrapy_path=/code/scrapers/manolo_scraper
@@ -8,4 +8,4 @@ scrapy_bin=/usr/local/bin/scrapy
 
 cd $scrapy_path
 
-$scrapy_bin crawl congreso -a date_start=$last_week -a date_end=$today
+$scrapy_bin crawl $2 -a date_start=$last_week -a date_end=$today
