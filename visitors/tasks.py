e9e3bc3f666045b2130baacd4168aef7bf1ededd
AniversarioPeru <aniversarioperu1@gmail.com>
2021-08-27 08:09:35 -0500

    run scraper count

--- visitors/tasks.py
+++ visitors/tasks.py
@@ -4,15 +4,17 @@ import logging
 from celery.task import periodic_task
 from django.db import OperationalError
 
+from visitors.management.commands.run_scraper_counts import run_counts
 from visitors.management.commands.run_statistics import run_statistics
 
 log = logging.getLogger(__name__)
 
 
-@periodic_task(run_every=timedelta(minutes=60 * 25))
+@periodic_task(run_every=timedelta(minutes=60 * 4))
 def run_stats() -> None:
     log.info("Recompute stats")
     try:
         run_statistics()
+        run_counts()
     except (OperationalError, Exception) as error:
         log.exception(f"Error while computing stats: {error}")
