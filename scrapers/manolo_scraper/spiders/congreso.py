786e6c5aafbda18cbc3656286a87b474047a2257
AniversarioPeru <aniversarioperu1@gmail.com>
2021-08-28 14:18:44 -0500

    fixed congreso

--- scrapers/manolo_scraper/spiders/congreso.py
+++ scrapers/manolo_scraper/spiders/congreso.py
@@ -1,130 +1,76 @@
 # -*- coding: utf-8 -*-
 import re
-import math
+import json
 
-from scrapy import FormRequest, Request
+from scrapy import Request
 
 from scrapers.manolo_scraper.spiders.spiders import ManoloBaseSpider
 from ..items import ManoloItem
-from ..item_loaders import ManoloItemLoader
 from ..utils import make_hash
 
 
 class CongresoSpider(ManoloBaseSpider):
     name = 'congreso'
-    allowed_domains = ['regvisitas.congreso.gob.pe']
-    NUMBER_OF_PAGES_PER_PAGE = 10
+    allowed_domains = ['wb2server.congreso.gob.pe']
+    base_url = 'https://wb2server.congreso.gob.pe/regvisitastransparencia/filtrar'
 
     def initial_request(self, date):
         date_str = date.strftime("%d/%m/%Y")
-
-        # This initial request always hits the current page of the date.
-        request = Request(url='http://regvisitas.congreso.gob.pe/regvisitastransparencia/',
-                          meta={
-                              'date': date_str,
-                          },
-                          dont_filter=True,
-                          callback=self.parse_initial_request)
-
-        return request
-
-    def parse_initial_request(self, response):
-        date = response.meta['date']
-        request = self._request_initial_date_page(response, date, self.parse_pages)
-        yield request
-
-    def _request_initial_date_page(self, response, date_str, callback):
-        formdata = {
-            'TxtFecha': date_str,
-            'BtnBuscar': 'Buscar'
+        payload = {
+            'empleadoCampo': '1',
+            'empleadoNombres': '',
+            'fechaDesde': date.strftime('%Y-%m-%d'),
+            'fechaHasta': date.strftime('%Y-%m-%d'),
+            'visitanteCampo': '1',
+            'visitanteNombres': ''
         }
 
-        request = FormRequest.from_response(response,
-                                            formdata=formdata,
-                                            dont_click=True,
-                                            dont_filter=True,
-                                            callback=callback
-                                            )
-
-        request.meta['date'] = date_str
-        request.meta['current_page'] = 1
+        # This initial request always hits the current page of the date.
+        request = Request(
+            url=self.base_url,
+            method='POST',
+            body=json.dumps(payload),
+            meta={
+                'date': date_str,
+            },
+            dont_filter=True,
+            callback=self.parse,
+        )
         return request
 
-    def parse_pages(self, response):
-        date = response.meta['date']
-
-        # Parse Items
-        items = self.parse(response)
-        for item in items:
-            yield item
-
-        request = self._request_next_page(response, date, self.parse_pages)
-
-        yield request
-
     def parse(self, response):
-        date = self.get_date_item(response.meta['date'], '%d/%m/%Y')
+        res_json = response.json()
 
-        for row in response.xpath('//table[@class="grid"]/tr'):
-            data = row.xpath('td')
+        for record in res_json:
+            date_str = self.get_date_item(response.meta['date'], '%d/%m/%Y')
 
-            if len(data) > 9:
-                full_name = data[2].xpath('./span/text()').extract_first(default='')
+            # extract hora vista from fecha visita
+            fecha_visita_orig = record.get('fechaVisitaRecepcion', '') or ''
+            fecha_visita = fecha_visita_orig.split(' ')
+            try:
+                hora_visita = fecha_visita[1]
+            except IndexError:
+                hora_visita = fecha_visita_orig
 
-                if full_name.strip():
-                    l = ManoloItemLoader(item=ManoloItem(), selector=row)
-                    l.add_value('institution', self.name)
-                    l.add_value('date', date)
-                    l.add_value('full_name', full_name)
+            location = record.get('ubicacion', '') or ''
+            location = re.sub(r'\s+', ' ', location)
 
-                    l.add_xpath('time_start', './td[2]/span/text()')
-                    l.add_xpath('id_document', './td[4]/span/text()')
-                    l.add_xpath('id_number', './td[5]/span/text()')
-                    l.add_xpath('entity', './td[6]/span/text()')
-                    l.add_xpath('reason', './td[7]/span/text()')
-                    l.add_xpath('host_name', './td[8]/span/text()')
-                    l.add_xpath('host_title', './td[9]/span/text()')
-                    l.add_xpath('office', './td[10]/span/text()')
-                    l.add_xpath('time_end', './td[11]/span/text()')
+            item = ManoloItem(
+                institution=self.name,
+                date=date_str,
+                full_name=record.get('entidadVisitanteNombreCompleto', '') or '',
+                time_start=hora_visita,
+                id_document=record.get('entidadVisitanteTipoDocumento', '') or '',
+                id_number=record.get('entidadVisitanteDocumento', '') or '',
+                entity=record.get('entidad', '') or '',
+                reason=record.get('motivo', '') or '',
+                host_name=record.get('empleado', '') or '',
+                host_title=record.get('cargo', '') or '',
+                location=location,
+                office=record.get('centroCostoNombre', '') or '',
+                meeting_place=record.get('edificio', '') or '',
+                time_end=record.get('fechaVisitaTermino', '') or '',
+            )
 
-                    item = l.load_item()
-                    item = make_hash(item)
-
-                    yield item
-
-    def _request_next_page(self, response, date_str, callback):
-        current_page = int(response.meta['current_page'])
-
-        total_string = response.css('#LblTotal').xpath('./text()').extract_first(default='')
-
-        total = re.search(r'(\d+)', total_string)
-
-        if total:
-            # Deal with the next page.
-            total = total.group(1)
-            number_of_pages = self._get_number_of_pages(int(total))
-
-            if current_page < number_of_pages:
-                current_page += 1
-
-                formdata = {
-                    'TxtFecha': date_str,
-                    'BtnBuscar': 'Buscar',
-                    'LwVisitasCR$DpVisitasCR$ctl02$ctl00.x': '1',
-                    'LwVisitasCR$DpVisitasCR$ctl02$ctl00.y': '1'
-                }
-
-                request = FormRequest.from_response(response,
-                                                    formdata=formdata,
-                                                    dont_click=True,
-                                                    dont_filter=True,
-                                                    callback=callback,
-                                                    )
-
-                request.meta['date'] = date_str
-                request.meta['current_page'] = current_page
-
-                return request
-
-    def _get_number_of_pages(self, total_of_records):
-        return int(math.ceil(total_of_records / float(self.NUMBER_OF_PAGES_PER_PAGE)))
+            item = make_hash(item)
+            yield item
--- scrapers/tests/test_congreso_spider.py
+++ scrapers/tests/test_congreso_spider.py
@@ -1,8 +1,8 @@
 import os
 import unittest
 
-from manolo_scraper.spiders.congreso import CongresoSpider
-from utils import fake_response_from_file
+from scrapers.manolo_scraper.spiders.congreso import CongresoSpider
+from scrapers.tests.utils import fake_response_from_file
 
 
 class TestCongresoSpider(unittest.TestCase):
@@ -11,26 +11,28 @@ class TestCongresoSpider(unittest.TestCase):
         self.spider = CongresoSpider()
 
     def test_parse_item(self):
-        filename = os.path.join('data/congreso', '18-08-2015.html')
+        filename = os.path.join('data/congreso', '2021-08-24.json')
         items = self.spider.parse(fake_response_from_file(filename, meta={'date': u'18/08/2015'}))
 
         item = next(items)
-        self.assertEqual(item.get('full_name'), u'ZEVALLOS FLOREZ, CESAR')
+        self.assertEqual(item.get('full_name'), u'RAFAEL PINTADO, HENRY HECTOR')
+        self.assertEqual(item.get('host_title'), u'AUXILIAR')
+        self.assertEqual(item.get('reason'), u'NORMAL')
+        self.assertEqual(item.get('time_start'), u'17:46')
         self.assertEqual(item.get('id_document'), u'DNI/LE')
-        self.assertEqual(item.get('id_number'), u'07632139')
-        self.assertEqual(item.get('entity'), u'EL ROCOTO')
-        self.assertEqual(item.get('reason'), u'PERSONAL DEL CONCESIONARIO')
-        self.assertEqual(item.get('host_name'), u'PEREYRA SALAZAR, WALTER')
-        self.assertEqual(item.get('office'), u'G.F. DE SERVICIO SOCIAL')
-        self.assertEqual(item.get('time_start'), u'08:15')
-        self.assertEqual(item.get('time_end'), None)
+        self.assertEqual(item.get('id_number'), u'06794905')
+        self.assertEqual(item.get('entity'), u'SIN INSTITUCION')
+        self.assertEqual(item.get('host_name'), u'ZUÑIGA PAREDES, BLANCA LEONOR')
+        self.assertEqual(item.get('office'), u'TERCERA VICEPRESIDENCIA')
+        self.assertEqual(item.get('meeting_place'), u'PALACIO')
+        self.assertEqual(item.get('location'), u'PALACIO : 229-E - TERCERA VICEPRESIDENCIA')
+        self.assertEqual(item.get('time_end'), '21:15')
         self.assertEqual(item.get('institution'), u'congreso')
-        self.assertEqual(item.get('title'), u'TECNICO ADMINISTRATIVO')
         self.assertEqual(item.get('date'), u'2015-08-18')
 
         number_of_items = 1 + sum(1 for x in items)
 
-        self.assertEqual(number_of_items, 10)
+        self.assertEqual(number_of_items, 2)
 
     def test_start_request(self):
         self.spider.date_start = '2015-08-18'
@@ -38,7 +40,9 @@ class TestCongresoSpider(unittest.TestCase):
         requests = self.spider.start_requests()
 
         request = next(requests)
-        self.assertEqual(request.url, 'http://regvisitas.congreso.gob.pe/regvisitastransparencia/')
+        self.assertEqual(
+            request.url, 'https://wb2server.congreso.gob.pe/regvisitastransparencia/filtrar'
+        )
         self.assertEqual(request.meta, {'date': '18/08/2015'})
 
         number_of_requests = sum(1 for _ in requests)
