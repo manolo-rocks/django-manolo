a061e74d1765255c057cae33166b0fef96be385c
AniversarioPeru <aniversarioperu1@gmail.com>
2022-10-20 07:36:00 -0500

    deactivate some cronjobs

--- visitors/tasks.py
+++ visitors/tasks.py
@@ -10,11 +10,12 @@ from visitors.management.commands.run_statistics import run_statistics
 log = logging.getLogger(__name__)
 
 
-@periodic_task(run_every=timedelta(minutes=60 * 4))
+@periodic_task(run_every=timedelta(minutes=60 * 24 * 7))
 def run_stats() -> None:
     log.info("Recompute stats")
     try:
-        run_statistics()
+        # Too CPU demanding
+        # run_statistics()
         run_counts()
     except (OperationalError, Exception) as error:
         log.exception(f"Error while computing stats: {error}")
