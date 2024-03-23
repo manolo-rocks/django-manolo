82693950684ac7b4e5a761319d87978254ccbe8f
Carlos Peña <mycalesis@gmail.com>
2024-03-22 21:10:45 -0500

    add postgres

--- manolo/settings/base.py
+++ manolo/settings/base.py
@@ -28,6 +28,18 @@ DATABASES = {
 }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
+
 ADMINS = (
     ('AniversarioPeru', 'aniversarioperu1@gmail.com'),
 )
