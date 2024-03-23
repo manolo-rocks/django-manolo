4cd29af1dd6eef6b6beb3265a5779680df1395b6
Carlos Peña <mycalesis@gmail.com>
2024-03-22 21:15:07 -0500

    add postgres

--- manolo/settings/local.py
+++ manolo/settings/local.py
@@ -30,7 +30,7 @@ if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'github_actions',
+            'NAME': 'manolo',
             'USER': 'postgres',
             'PASSWORD': 'postgres',
             'HOST': '127.0.0.1',
