64a2f602c8fd170669c5313dcd11f2105c083e3e
Carlos Peña <mycalesis@gmail.com>
2024-03-28 19:02:16 -0500

    rm obsolete code from cazador

--- cazador/views.py
+++ cazador/views.py
@@ -1,39 +1,7 @@
-import urllib
-
 from django.shortcuts import render
-from django.utils.datastructures import MultiValueDictKeyError
 from django.views.decorators.csrf import csrf_exempt
-
-from cazador.forms import CazadorForm
-from visitors.views import do_pagination
 
 
 @csrf_exempt
 def index(request):
-    try:
-        original_query = request.GET['q']
-    except MultiValueDictKeyError:
-        return render(request, "cazador/index.html")
-
-    all_items = []
-    query_list = original_query.splitlines()
-    for query in query_list:
-        request_query = {'q': query}
-        form = CazadorForm(request_query)
-        query_results = form.search()
-        all_items += query_results
-
-    search_queryset = list(set(all_items))
-    paginator, page = do_pagination(request, search_queryset)
-
-    return render(
-        request,
-        "cazador/results.html",
-        context={
-            "paginator": paginator,
-            "page": page,
-            "query": "; ".join(query_list),
-            "original_query": original_query,
-            "encoded_query": urllib.parse.quote_plus(original_query.strip()),
-        }
-    )
+    return render(request, "cazador/index.html")
