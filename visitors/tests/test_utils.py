8fe8eca7f756787c84e5a87f4ce4d88a03a924e2
Bayta Darell <bayta.darell.1400@gmail.com>
2025-05-10 01:33:16 -0500

    progress

--- visitors/tests/test_utils.py
+++ visitors/tests/test_utils.py
@@ -2,22 +2,10 @@ from unittest.mock import MagicMock
 
 from django.test import TestCase
 
-from visitors.utils import get_user_profile, is_dni, Paginator
+from visitors.utils import is_dni, Paginator
 
 
 class TestUtils(TestCase):
-
-    def test_get_user_profile__user_is_authenticated_has_0_credits(self):
-        mock_subscriber = MagicMock()
-        mock_subscriber.credits = 0
-
-        mock_request = MagicMock()
-        mock_request.user.is_authenticated.return_value = True
-        mock_request.user.subscriber = mock_subscriber
-        result = get_user_profile(mock_request)
-        self.assertEqual(True, result["expired"])
-        self.assertEqual(False, result["about_to_expire"])
-        self.assertEqual(0, result["credits"])
 
     def test_is_dni(self):
         items = [
