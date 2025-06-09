717b4c7d6cef957b9aa4d335d0f845238905afbb
Bayta Darell <bayta.darell.1400@gmail.com>
2025-06-08 23:57:31 -0500

    coment migration

--- visitors/migrations/0034_institution_ruc.py
+++ visitors/migrations/0034_institution_ruc.py
@@ -10,9 +10,9 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name="institution",
-            name="ruc",
-            field=models.CharField(max_length=200, null=True, unique=True),
-        ),
+        # migrations.AddField(
+        #     model_name="institution",
+        #     name="ruc",
+        #     field=models.CharField(max_length=200, null=True, unique=True),
+        # ),
     ]
